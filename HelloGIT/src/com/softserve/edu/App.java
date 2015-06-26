36e4e01b39fdc64c9c0a31886461bbf20ff95726
vblyfqn <vblyfqn.@gmail.com>
2015-06-26 18:15:00 +0300

    commit #3 into master

--- HelloGIT/src/com/softserve/edu/App.java
+++ HelloGIT/src/com/softserve/edu/App.java
@@ -1,9 +1,11 @@
 package com.softserve.edu;
 
-/**
- * Hello world!
- *
- */
+class Figure {
+    public double getPerimeter(double a, double b) {
+        return 2*(a+b);
+    }
+}
+
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello master!" );
